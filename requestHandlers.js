942df4c00c7fb6d97dbd7bbbe7c84b02fa73a91b
Ivan Rios <ivanrios.a@gmail.com>
2016-04-07 23:54:14 -0500

    Ejemplo de Post

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,22 +1,31 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function iniciar(response) {
-  console.log("Controlador de petición 'iniciar' fue llamado.");
+function iniciar(response, postData) {
+  console.log("Controlador de peticion 'inicio' fue llamado.");
 
-  exec("find /",
-    { timeout: 10000, maxBuffer: 2000*1024 },
-    function (error, stdout, stderr) {
-      response.writeHead(200, {"Content-Type": "text/html"});
-      response.write(stdout);
-      response.end();
-    });
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/subir" method="post">'+
+    '<textarea name="text" rows="5" cols="40"></textarea><br />'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
-function subir(response) {
-  console.log("Controlador de petición 'subir' fue llamado.");
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Subir");
-  response.end();
+function subir(response, dataPosteada) {
+    console.log("Controlador de peticion 'subir' fue llamado.");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Tu enviaste el texto: : " +
+    querystring.parse(dataPosteada)["text"]);
+    response.end();
 }
 
 exports.iniciar = iniciar;
