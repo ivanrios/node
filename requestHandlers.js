abaca8ce46c49129e344a0bc480eb5cb21206681
Ivan Rios <ivanrios.a@gmail.com>
2016-04-07 23:32:43 -0500

    Proceso Bloqueante

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,19 @@
 function iniciar() {
-  console.log("Controlador de peticion 'iniciar' fue llamado.");
+  console.log("Manipulador de peticion 'iniciar' fue llamado.");
+
+  function sleep(milliSeconds) {  
+    // obten la hora actual
+    var startTime = new Date().getTime();
+    // atasca la cpu
+    while (new Date().getTime() < startTime + milliSeconds); 
+  }
+
+  sleep(10000);
   return "Hola Iniciar";
 }
 
 function subir() {
-  console.log("Controlador de peticion 'subir' fue llamado.");
+  console.log("Manipulador de peticion 'subir' fue llamado.");
   return "Hola Subir";
 }
 
