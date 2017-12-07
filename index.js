dabd84e97cd474b3123312a6c10e485b92fe5447
Ivan Rios <ivanrios.a@gmail.com>
2017-12-06 23:09:10 -0500

    Se agrega el ejemplo

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
+var request = require("request")
+var express = require('express');
+var app = express();
 
-var handle = {}
+app.get('/', function (req, res) {
+  
+	var url = 'https://www.easports.com/fifa/ultimate-team/api/fut/item?page=1'
+	var datos = '';
+	request.get(url, function(err, response, data){
+		if (err)
+			console.log(err)
+		else{
+			jugadores = JSON.parse(data)
+			jugadores.items.forEach(function(elem){
+				
+				datos += "<li><img src='"+elem.headshotImgUrl+"'/> "+elem.name+"</li>"
 
-handle["/"]        = requestHandlers.iniciar;
-handle["/iniciar"] = requestHandlers.iniciar;
-handle["/subir"]   = requestHandlers.subir;
+			})
+			res.send(datos)
+		}
 
-server.iniciar(router.route, handle);
+	})	
+
+});
+
+app.listen(3000, function () {
+  console.log('Escuchando por el puerto 3000!');
+});
+
+
