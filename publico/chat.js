6bf7fe46b5831a78007d3242fca89321c150a9a9
Ivan Rios <ivanrios.a@gmail.com>
2017-12-11 20:59:52 -0500

    Se termina el chat

--- publico/chat.js
+++ publico/chat.js
@@ -4,8 +4,22 @@
    $(document).ready(function(){
      
      $('form').submit(function(){
+     	var mensaje = $("#m").val();
+     	var fecha = new Date();
+     	var data = {mensaje:mensaje, fecha:fecha}
+     	socket.emit('mensaje', data);
+     	$("#m").val('');
        return false;
     });
+
+
+     socket.on('mensaje', function(msg){
+     	var fecha = new Date(msg.fecha).toISOString();
+     	var linea = "<li><span title='"+fecha+
+     	            "' class='timeago'></span><br/>"+msg.mensaje+"</li>";
+     	$("#mensajes").append(linea)
+     	$('span.timeago').timeago();
+     })
 
 
    });
