4866f7d527b60e0c3ca660c12f175ef869c66db4
Ivan Rios <ivanrios.a@gmail.com>
2017-12-11 21:19:18 -0500

    Json Server

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+/*
+
+$ npm install -g json-server
 
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/publico/index.html');
-});
 
-app.use(express.static(__dirname + '/publico'));
+$ json-server --watch db.json
 
 
-io.on('connection', function(socket){
-	console.log("Un usuario conectado")
-
-	socket.on('disconnect', function(){
-		console.log("Un usuario desconectado")
-	});
-
-	socket.on('mensaje', function(msg){
-		console.log("Un nuevo mensaje: ", msg);
-		io.emit('mensaje', msg);
-	})
-
-});
-
-http.listen(3000, function(){
-  console.log('Escuchando por el puerto localhost:3000');
-});
-
+*/
