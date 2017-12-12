d65d8db1d1ead0527c54076be1e7ff76e04a687a
Ivan Rios <ivanrios.a@gmail.com>
2017-12-11 21:59:22 -0500

    Se agrega el codigo

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
-/*
+var express = require('express');
+var fs  = require('fs');
+var app = express();
+var bodyParser = require('body-parser')
 
-$ npm install -g json-server
+var db = JSON.parse(fs.readFileSync("db.json").toString());
 
 
 
-$ json-server --watch db.json
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+	console.log("Index");
+	res.send({res:"Ok"});
+});
+
+app.get('/eventos', function(req, res){
+	console.log("Todos los eventos");
+	res.send(db.eventos);
+});
+
+app.get('/eventos/:evento', function(req, res){
+	var evento = req.params.evento
+	console.log("Detalles de evento");
+	var busqueda= db.eventos.filter(function(elem){return (elem.id==evento)})
+	res.send(busqueda);
+});
+
+app.put('/eventos/:evento', function(req, res){
+	var evento = req.params.evento;
+	var nombre = req.body.evento;
+	var autor = req.body.autor;
+
+	console.log("Actualiza el evento");
+	db.eventos = db.eventos.map(function(elem){
+		if (elem.id==evento){
+			elem.evento = nombre;
+			elem.autor  = autor;
+		}
+		return elem;
+	});
+	var busqueda= db.eventos.filter(function(elem){return (elem.id==evento)})
+	res.send(busqueda);
+});
+
+app.delete('/eventos/:evento', function(req, res){
+	var evento = req.params.evento;
+	console.log("Elimina el evento");
+	db.eventos = db.eventos.filter(function(elem, i, object){
+		if (elem.id!=evento)
+			return elem;
+	});
+	var busqueda= db.eventos.filter(function(elem){return (elem.id==evento)})
+	res.send(busqueda);
+});
 
 
-*/
+app.listen(3000, function(){
+  console.log('Escuchando por el puerto localhost:3000');
+});
+
